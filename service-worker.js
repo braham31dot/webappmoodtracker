4999d8f9ffe221b2f7ece846333ed7b39d3528d2
braham31dot <braham.ben31@gmail.com>
2025-04-09 15:53:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 const CACHE_NAME = 'mood-tracker-cache-v1';
 const urlsToCache = [
-  '/',                     // Ensure the root path is cached
-  '/index.html',            // Cache the HTML page
-  '/manifest.json',         // Cache the manifest for PWA features
-  '/logo.png',              // Cache the logo image
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png',
+  '/service-worker.js', 
 ];
 
 // Cache files on install
