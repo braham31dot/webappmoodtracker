c5a5014c74dae9b30f290a465dbe61e2b4b8af55
braham31dot <braham.ben31@gmail.com>
2025-04-09 15:57:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/logo.png',
-  '/service-worker.js', 
+  '/service-worker.js',
 ];
 
 // Cache files on install
@@ -23,7 +23,14 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        // If the resource is found in the cache, serve it. Otherwise, fetch it from the network.
+        return response || fetch(event.request).then((fetchResponse) => {
+          // Optionally, cache the response here for later use
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
       })
   );
 });
